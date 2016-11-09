9a2281cb85eb6c5d0c89c15de6d26dac9bf20e8c
dolwin davis <dolwin davis>
2016-11-09 12:18:12 +0530

    composer installed and autoloaded files using autoload.php

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,11 +4,14 @@ $app['config'] = require 'config.php';//config.php contais database details and 
 // $app=[
 	//'config'=>database[]
 //]
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-require 'core/Request.php';
-require 'core/Router.php';
+//require 'core/database/Connection.php';
+//require 'core/database/QueryBuilder.php';
+//require 'core/Request.php';
+//require 'core/Router.php';
 
-$app['database'] = new QueryBuilder(Connection::make($app['config']['database']));
+$app['database'] = new QueryBuilder(
+
+	Connection::make($app['config']['database'])
+		);
 
  ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+require 'vendor/autoload.php';
 require 'core/bootstrap.php';
 // die(var_dump($app));
 // $router = new Router;
